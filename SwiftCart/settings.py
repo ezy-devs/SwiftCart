8281088ee554e261ab59659c43880d895fd122d8
nzorov_emmanuel <nzorovemmanuel@gmail.com>
2025-01-13 15:52:40 +0100

    fix: update database settings to use environment variables and correct order link

--- SwiftCart/settings.py
+++ SwiftCart/settings.py
@@ -96,15 +96,14 @@ WSGI_APPLICATION = 'SwiftCart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  
-        'NAME': 'swift-cart-db',  
-        'USER': 'postgres',  
-        'PASSWORD': 'ezydevs@2003',  
-        'HOST': 'localhost',  
-        'PORT': '5432',
+        'ENGINE': env('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST', default='localhost'),
+        'PORT': env('DB_PORT', default='5432'),
     },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
